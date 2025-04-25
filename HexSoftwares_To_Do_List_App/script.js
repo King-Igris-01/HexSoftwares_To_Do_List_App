20cdd36139933c697637f024c440f0dceb060070
Immanuel <kgaogelonkwana72@gmail.com>
2025-04-25 21:00:17 +0200

    Updated script.js with code from local

--- HexSoftwares_To_Do_List_App/script.js
+++ HexSoftwares_To_Do_List_App/script.js
@@ -2,6 +2,7 @@ const inputBox = document.getElementById('inputBox');
 const addBtn = document.getElementById('addBtn');
 const todoList = document.getElementById('todoList');
 
+let editTodo = null;
 
 
 const addTodo = () => {
@@ -11,28 +12,52 @@ const addTodo = () => {
         alert('Forgot to add a task! Try again!');
         return false;
     }
+    else{
+        
+    }
 
+
+    if(addBtn.value === "Edit"){
+        editTodo.target.previousElementSibling.innerHTML = inputText;
+        addBtn.value = "Add";
+        inputBox.value = "";
+    }
+ 
     //Create a new list item / p tag
     const li = document.createElement('li');
     const p = document.createElement('p');
     p.innerText = inputText;
     li.appendChild(p);
 
-    //Create a delete button
-    const deleteBtn = document.createElement('button');
-    deleteBtn.innerText = 'Delete';
-    li.appendChild(deleteBtn);
-
     //Create an edit button
-    //Create a delete button
     const editBtn = document.createElement('button');
-    deleteBtn.innerText = 'Edit';
+    editBtn.innerText = 'Edit';
+    editBtn.classList.add("btn", "editBtn");
     li.appendChild(editBtn);
+
+     //Create a delete button
+     const deleteBtn = document.createElement('button');
+     deleteBtn.innerText = 'Delete';
+     deleteBtn.classList.add("btn", "deleteBtn");
+     li.appendChild(deleteBtn);
     
 
     todoList.appendChild(li);
     inputBox.value = "";
 }
 
+const updateTodo = (e) => {
+    if(e.target.innerHTML === "Delete"){
+        todoList.removeChild(e.target.parentElement);
+    }
+    if(e.target.innerHTML === "Edit"){
+        inputBox.value = e.target.previousElementSibling.innerHTML;
+        inputBox.focus();
+        addBtn.value = "Edit";
+    }
+}
+
+   
 addBtn.addEventListener('click', addTodo);
+todoList.addEventListener('click', updateTodo );
 
